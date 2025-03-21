9f158cc60459cd48260b5133162735b6259a1b40
Éric <deck.martins.silva@gmail.com>
2025-03-21 09:00:46 -0300

    Create Aula 21.03 - Exemplo 2.java

---
+++ Estrutura de dados/Aula 21.03 - Exemplo 2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Nomes {
+    public static void main(String[] args) {
+        String nomeCompleto;
+        Scanner teclado = new Scanner(System.in);
+        String vetorNome[];
+
+        do{
+        System.out.print("Nome completo: ");
+        nomeCompleto = teclado.nextLine();
+        nomeCompleto = nomeCompleto.toUpperCase();
+        vetorNome = nomeCompleto.split(" ");
+        } while (vetorNome.length < 2);
+
+        String email = vetorNome[0] + "." + vetorNome[vetorNome.length - 1] + "@ufn.edu.br";
+        email = email.toLowerCase();
+        System.out.println(email);
+        
+        System.out.println(vetorNome[vetorNome.length - 1]);
+        //for (i=0; i<vetorNome.length; i++){
+        //    System.out.println(vetorNome[i]);
+        //}
+
+        //System.out.println(nomeCompleto);
+
+        teclado.close();
+    }
+}
